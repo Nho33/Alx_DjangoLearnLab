8823dd919bf2bc0156863336d248281e9147182f
Nonopha <nonophast@gmail.com>
2024-09-01 06:20:15 -0700

    Updated the urls.py script

--- django-models/relationship_app/templates/urls.py
+++ django-models/relationship_app/templates/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import register
+import relationship_app.views as views
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('admin-view/', views.admin_view, name='admin_view'),
+    path('librarian-view/', views.librarian_view, name='librarian_view'),
+    path('member-view/', views.member_view, name='member_view'),
+    path('add_book/', views.add_book, name='add_book'),  # URL pattern for adding a book
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),  # URL pattern for editing a book  # URL pattern for editing a book
+    path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'),
 ]
