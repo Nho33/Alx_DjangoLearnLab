4037abbd1634a1b8962c9ced23c214b33124d962
nho33 <nonophast@gmail.com>
2024-09-13 18:10:34 -0700

    Created models for Author and Book

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length = 255)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length = 200)
+    publication_year = models.InterField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    def __str__(self):
+        return f"{self.title}, ({self.publicatio})"
